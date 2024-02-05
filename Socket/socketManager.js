813365b25ce55eabd808f67398b0a226c23e6735
priyanshu kumar <dlnpriyanshu@gmail.com>
2024-02-05 07:11:51 +0530

    socket connection

---
+++ Socket/socketManager.js
@@ -0,0 +1,44 @@
+const fetchuser = require("../middleware/fetchuser");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "mediaBook-2.O";
+// const data = jwt.verify(localStorage.getItem("auth-token"), JWT_SECRET);
+const onlineUsers =[];
+const SocketManager = (server) =>
+{
+    const socketIO = require("socket.io")
+    const io = socketIO(server, {
+        cors: {
+          origin: "http://localhost:3000", 
+          methods: ["GET", "POST"]
+        }
+      });
+
+    io.on('connection', (socket) => {
+        console.log('A user connected',socket.id);
+  
+    const authToken = socket.handshake.auth.token;
+    if (authToken) {
+        try {
+            const decoded = jwt.verify(authToken, JWT_SECRET);
+            const userId = decoded.user.id;
+            onlineUsers.push({id:userId,socketId:socket.id})
+            // console.log(onlineUsers)
+            io.emit('newUser', userId);
+        } catch (error) {
+            console.error('Error verifying JWT token:', error.message);
+        }
+    } else {
+        console.error('No authentication token provided.');
+    }
+        socket.on('disconnect', () => {
+            console.log('User disconnected');
+            const indexToRemove = onlineUsers.findIndex(obj => obj.socketId === socket.id);
+            if (indexToRemove !== -1) {
+                onlineUsers.splice(indexToRemove, 1);
+
+                // console.log(onlineUsers)
+    }
+        });
+    });
+}
+module.exports = SocketManager
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express=require('express');
+const http = require("http")
+const socketIO = require("socket.io")
 require("dotenv").config();
 var cors=require('cors')
 const bodyParser=require("body-parser")
 const app=express();
+const server = http.createServer(app);
+const SocketManager = require('./Socket/socketManager')
+SocketManager(server)
 const port=process.env.port || 5000;
 const connectToMongo=require('./dbConnect/db');
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json({limit: '50mb'}))
+
 
 app.use(cors());
 app.use(express.json());
@@ -15,8 +21,12 @@ app.use('/api/auth',require('./routes/auth'));
 app.use('/api/post',require('./routes/post'));
 app.use('/api/user',require('./routes/user'));
 
-app.listen(port,()=>{
-    console.log(`it is listening at ${port}`);
-})
+// app.listen(port,()=>{
+//     console.log(`it is listening at ${port}`);
+// })
 
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 connectToMongo(process.env.dburl);
