d35ed457dcf7181a8de381b84877aaa8317c9552
Priyanshu Kumar <114975117+Priyanshu-kr-gupta@users.noreply.github.com>
2024-02-21 08:33:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,39 @@
-  const connectToMongo=require('./dbConnect/db');
-const express=require('express');
- const cors=require('cors')
- require("dotenv").config();
- const bodyParser=require("body-parser")
+const connectToMongo = require('./dbConnect/db');
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
-  connectToMongo(process.env.dburl);
+// Database connection
+connectToMongo(process.env.dburl);
 
-  const app=express();
-  const port=process.env.port || 5000;
+// Create Express app
+const app = express();
+const port = process.env.port || 5000;
 
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json({ limit: '50mb', extended: true }))
-app.use(express.json())
-app.use(
-  cors()
-);
-app.use(cors({
-  origin: "https://media-book-frontend-two.vercel.app",
-  methods: ["GET", "POST", "PUT"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(cors());
 
+// Require http and socket.io modules
+const http = require("http");
+const socketIO = require("socket.io");
 
-  const http = require("http")
-  const socketIO = require("socket.io")
- 
- 
- 
+// Create http server and socket.io instance
+const server = http.createServer(app);
+const io = socketIO(server);
 
+// Socket manager
+const { SocketManager } = require('./Socket/socketManager');
+const { GlobalChatSocketManager } = require('./Socket/GlobalChatSocketManager');
+SocketManager(server);
 
-  const server = http.createServer(app);
-  const {SocketManager} = require('./Socket/socketManager')
-  const {GlobalChatSocketManager}=require('./Socket/GlobalChatSocketManager')
-  SocketManager(server)
+// Define routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/post', require('./routes/post'));
+app.use('/api/user', require('./routes/user'));
 
-
-  // app.use(cors({
-  //   origin: "http://localhost:3000",
-  //   methods: ["GET", "POST","PUT"]
-  // }));
-
-
-  app.use('/api/auth',require('./routes/auth'));
-  app.use('/api/post',require('./routes/post'));
-  app.use('/api/user',require('./routes/user'));
-
-  // app.listen(port,()=>{
-  //     console.log(`it is listening at ${port}`);
-  // })
-
-
-  server.listen(port, () => {
+// Start the server
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
-
+});
