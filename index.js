fff9475c1f6c75c547f491726d4e82c0c8a63399
Priyanshu Kumar <114975117+Priyanshu-kr-gupta@users.noreply.github.com>
2024-02-21 08:34:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-const connectToMongo = require('./dbConnect/db');
-const express = require('express');
-const cors = require('cors');
-require("dotenv").config();
-const bodyParser = require("body-parser");
+  const express=require('express');
+  const http = require("http")
+  const socketIO = require("socket.io")
+  require("dotenv").config();
+  const cors=require('cors')
+  const bodyParser=require("body-parser")
+  const app=express();
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.use(express.json({limit: '50mb'}))
+  app.use(cors());
+  app.use(express.json());
+  const server = http.createServer(app);
+  const {SocketManager} = require('./Socket/socketManager')
+  const {GlobalChatSocketManager}=require('./Socket/GlobalChatSocketManager')
+  SocketManager(server)
+  const port=process.env.port || 5000;
+  const connectToMongo=require('./dbConnect/db');
+ 
+  // app.use(cors({
+  //   origin: "http://localhost:3000",
+  //   methods: ["GET", "POST","PUT"]
+  // }));
 
-// Database connection
-connectToMongo(process.env.dburl);
 
-// Create Express app
-const app = express();
-const port = process.env.port || 5000;
+  app.use('/api/auth',require('./routes/auth'));
+  app.use('/api/post',require('./routes/post'));
+  app.use('/api/user',require('./routes/user'));
 
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json({ limit: '50mb', extended: true }));
-app.use(cors());
+  // app.listen(port,()=>{
+  //     console.log(`it is listening at ${port}`);
+  // })
 
-// Require http and socket.io modules
-const http = require("http");
-const socketIO = require("socket.io");
 
-// Create http server and socket.io instance
-const server = http.createServer(app);
-const io = socketIO(server);
-
-// Socket manager
-const { SocketManager } = require('./Socket/socketManager');
-const { GlobalChatSocketManager } = require('./Socket/GlobalChatSocketManager');
-SocketManager(server);
-
-// Define routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/post', require('./routes/post'));
-app.use('/api/user', require('./routes/user'));
-
-// Start the server
-server.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+  });
+  connectToMongo(process.env.dburl);
